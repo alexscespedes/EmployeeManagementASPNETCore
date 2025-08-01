a585dc2ae85249cd2ef77a28191c0c5dd6c342a2
Alexander Sencion Cespedes <alexandersenc99@gmail.com>
2025-07-31 20:06:51 -0400

    Create and configure AppDbContext

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,21 @@
+using System;
+using EmployeeManagement.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EmployeeManagement.Data;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+    public DbSet<Employee> Employees { get; set; }
+
+    public DbSet<Department> Departments { get; set; }
+
+    public DbSet<Position> Positions { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+    }
+}
