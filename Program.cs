2f8ef206f1c71093a09754505aeb869366a42a6e
Alexander Sencion Cespedes <alexandersenc99@gmail.com>
2025-08-04 18:38:41 -0400

    Add Custom Error Pages (404 and 500)

--- Program.cs
+++ Program.cs
@@ -18,13 +18,8 @@ using (var scope = app.Services.CreateScope())
     DbInitializer.Seed(context);
 }
 
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
-}
+app.UseExceptionHandler("/Home/Error");
+app.UseStatusCodePagesWithReExecute("/Home/StatusCode", "?code={0}");
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
